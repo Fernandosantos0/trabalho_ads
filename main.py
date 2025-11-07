9a04759b577c0db65e2868d8552fc05cc40a66e1
Fernando Santos <fernandomeca7@gmail.com>
2025-11-07 09:17:47 -0300

    Començando o trabalho

---
+++ main.py
@@ -0,0 +1,18 @@
+import os
+
+opcao = 1
+
+while opcao != '4':
+    print('*' * 40)
+    print('Sistema de Cadastro de Roupas'.upper())
+    print('*' * 40)
+    print('1 - Cadastrar Roupa')
+    print('2 - Listar Roupas Cadastradas')
+    print('3 - Apagar Cadastro')
+    print('4 - Sair')
+    
+    opcao = input('Digite a opção: ')
+    
+    
+# Limpar o terminal no em multiplataforma (Windows, Linux e macOs)
+os.system('cls' if os.name == 'nt' else 'clear')
